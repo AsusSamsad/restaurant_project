dc5d5a4378d4878126c90591c06b5af7afdf4fd5
AsusSamsad <sikandarkhan0785@gmail.com>
2025-04-26 16:51:09 +0530

    Update settings.py

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # 🛠️ Static files ka path set karein
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # 👈 Ye Render me kaam aayega
 
